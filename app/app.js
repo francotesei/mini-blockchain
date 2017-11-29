8970eb9f2870f46fb0eaef5dea5d4e339e16fa23
francotesei <ftesei96@gmail.com>
2017-11-28 22:50:20 -0300

    added ipAddr

--- app/app.js
+++ app/app.js
@@ -1,14 +1,15 @@
 import {default as express} from 'express';
 import {default as bodyParser} from 'body-parser';
 import {default as morgan} from 'morgan'
-import {default as routes} from './routes/index';
 import {default as WebSocket} from 'ws';
-import {default as discovery } from 'dns-discovery';
-import P2P from './model/P2P'
+import {default as discovery} from 'dns-discovery';
+import {default as ip} from 'ip';
+import {default as routes} from './routes/index';
+import P2P from './model/P2P';
 
 const http_port = process.env.HTTP_PORT || 3001;
 const p2p_port = process.env.P2P_PORT || 6001;
-const discoverPeers = process.env.DISCOVER_PEERS || false;
+const discoverPeers = process.env.DISCOVER_PEERS || true;
 const initialPeers = process.env.PEERS
   ? process.env.PEERS.split(',')
   : [];
@@ -24,24 +25,26 @@ var initHttpServer = () => {
 var initP2PServer = () => {
   var server = new WebSocket.Server({port: p2p_port});
   var myServ = discovery();
-  server.on('connection', ws => P2P.initConnection({ws:ws}));
-  // announce an app
-  myServ.announce('node-app', p2p_port)
+  server.on('connection', ws => P2P.initConnection({ws: ws}));
+  myServ.announce('node-app', p2p_port) // announce an app
   console.log('listening websocket p2p port on: ' + p2p_port);
 };
 
-var initDiscoveryPeers = ()=> {
+var initDiscoveryPeers = () => {
   var disc = discovery();
-  disc.on('peer', function (name, peer) {
-    console.log("New Peer Added");
+  disc.on('peer', function(name, peer) {
+    if (peer.host == ip.address() && peer.port == p2p_port) return;
+    console.log("New peer discovered");
     console.log(name, peer);
-    P2P.connectToPeers({peers:['ws://'+peer.host+':'+peer.port]});
+    P2P.connectToPeers({
+      peers: ['ws://' + peer.host + ':' + peer.port]
+    });
+    console.log("New Peer Added");
   });
 };
 
-
-
-P2P.connectToPeers({peers:initialPeers});
+P2P.connectToPeers({peers: initialPeers});
 initHttpServer();
 initP2PServer();
-if(discoverPeers) initDiscoveryPeers();
+if (discoverPeers)
+  initDiscoveryPeers();
